2670bd13d5fc566e70b4419a1caec5c7a47aa6e6
krcredo <krcredo@gmail.com>
2012-01-27 18:12:30 -0500

    User model with passwords, and user show page

---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+Factory.define :user do |user|
+  user.name                  "Xul Solo"
+  user.email                 "xsolo@example.com"
+  user.password              "foobar"
+  user.password_confirmation "foobar"
+end
+
